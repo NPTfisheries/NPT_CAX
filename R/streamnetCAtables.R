e4c6a4d0093b4623968ce018e7caa712667e6f76
Mike Ackerman <dirtpathwanderer@gmail.com>
2026-02-04 13:41:03 -0700

    removing functions after finding rCAX

--- R/streamnetCAtables.R
+++
@@ -1,9 +0,0 @@
-streamnetCAtables = function(api_key = Sys.getenv("STREAMNET_API_KEY", unset = NA_character_),
-                             base_url = "https://api.streamnet.org") {
-  streamnetFetchAllPages(
-    path = "api/v1/ca/tables",
-    api_key = api_key,
-    base_url = base_url,
-    all_pages = FALSE
-  )
-}
